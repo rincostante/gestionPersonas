2fb4acd8ed637495c45d51aaa387bc3aa52c5c79
rodriguezn <rodriguezn@L3000.MEDIOAMBIENTE.GOV.AR>
2015-06-17 17:35:47 -0300

    MbDomicilio

---
+++ src/main/java/ar/gob/ambiente/servicios/gestionpersonas/managedBeans/MbUsuario.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package ar.gob.ambiente.servicios.gestionpersonas.managedBeans;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+
+/**
+ *
+ * @author rodriguezn
+ */
+@ManagedBean(name = "mbUsuario1")
+@RequestScoped
+public class MbUsuario {
+
+    /**
+     * Creates a new instance of MbUsuario
+     */
+    public MbUsuario() {
+    }
+    
+}
